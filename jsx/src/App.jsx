01a4f8d94ff38a0e60ae6b3c54070137c79291e3
kishorekumar1111 <kishore.kumar@kalvium.community>
2024-03-02 11:37:36 +0530

    done

---
+++ jsx/src/App.jsx
@@ -0,0 +1,23 @@
+import "./App.css";
+import imageData from "./components/DataComponent";
+
+function App() {
+  const data = imageData();
+
+  return (
+    <div>
+      <h1>USING FUNCTIONAL COMPONENT</h1>
+      <div className="grid">
+        {data.map((el) => {
+          return (
+            <div key={el.id}>
+              <img src={el.img} />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ jsx/src/AppClass.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import imageData from "./components/DataComponent";
+
+export class AppClass extends Component {
+  render() {
+    return (
+      <div>
+        <h1>USING CLASS COMPONENT</h1>
+        <div className="grid">
+          {imageData().map((el) => {
+            return (
+              <div id={el.id}>
+                <img src={el.img} alt="" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default AppClass;
